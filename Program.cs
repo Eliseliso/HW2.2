cc4c0f95b0d16be316eaef4b06fc349ef260e780
Elis <eliso1190@yandex.ru>
2023-01-24 02:06:23 +0300

    First

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int num2 = num % 10;
+if (num > 99)
+{
+    num = num / 10;
+    Console.WriteLine(num2);
+}
+else
+if (num < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
